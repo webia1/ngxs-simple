86435707a5e08540db01e68d00742a72e3c84604
Erkan Buelbuel <erkan.buelbuel@bit.admin.ch>
2021-08-16 13:04:18 +0200

    Initial State

--- apps/ngx-simple/src/app/app.module.ts
+++ apps/ngx-simple/src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxsModule } from '@ngxs/store';
+import { environment } from './../environments/environment';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    NgxsModule.forRoot([], {
+      developmentMode: !environment.production,
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
