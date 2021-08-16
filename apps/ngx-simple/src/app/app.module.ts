40598be9054be2af093ea915d530ef6feac13523
Erkan Buelbuel <erkan.buelbuel@bit.admin.ch>
2021-08-16 13:39:14 +0200

    NGXS DEV Plugins

--- apps/ngx-simple/src/app/app.module.ts
+++ apps/ngx-simple/src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { NgxsModule } from '@ngxs/store';
 import { environment } from './../environments/environment';
+
+// NGXS RELATED STUFF
+
+import { NgxsModule } from '@ngxs/store';
+import { NgxsRouterPluginModule } from '@ngxs/router-plugin';
+import { NgxsFormPluginModule } from '@ngxs/form-plugin';
+import { NgxsStoragePluginModule } from '@ngxs/storage-plugin';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsDispatchPluginModule } from '@ngxs-labs/dispatch-decorator';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
@@ -15,6 +23,11 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
     NgxsModule.forRoot([], {
       developmentMode: !environment.production,
     }),
+    NgxsRouterPluginModule.forRoot(),
+    NgxsFormPluginModule.forRoot(),
+    NgxsStoragePluginModule.forRoot(),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+    NgxsDispatchPluginModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
