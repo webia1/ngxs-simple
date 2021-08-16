c091669a264baa4f72a51817fe2fc9524905da8c
Erkan Buelbuel <erkan.buelbuel@bit.admin.ch>
2021-08-16 13:26:22 +0200

    Empty Component Dashboard and Route root

--- apps/ngx-simple/src/app/app.module.ts
+++ apps/ngx-simple/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 import { NgxsModule } from '@ngxs/store';
 import { environment } from './../environments/environment';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DashboardComponent],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     NgxsModule.forRoot([], {
       developmentMode: !environment.production,
     }),
